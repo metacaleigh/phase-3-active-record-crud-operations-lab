83ee729c3001adae0385541bb9433c6bd328ed1d
caleigh <caleighsteill@outlook.com>
2023-02-04 19:07:22 -0700

    commit tree 064ad1e8339ac8a794d75de240b58792741e2d7b

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,29 @@
 class Movie < ActiveRecord::Base
+    def self.find_all_movies_by_year(year)
+        Movie.where(year: year)
+    end
 
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movies_after_2002(year)
+        Movie.where(year: year > 2002)
+    end 
 end
